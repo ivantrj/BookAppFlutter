6a3dd4a164e362f8a8829cd8776cedbc96e661b1
Ivan Trajanovski <ivantrajanovski@gmail.com>
2024-03-17 14:15:12 +0100

    migrate to sqflite

---
+++ lib/data/database_helper.dart
@@ -0,0 +1,43 @@
+import 'package:flutter_tabs_starter/data/book.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('books.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  Future _createDB(Database db, int version) async {
+    const idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+    const textType = 'TEXT NOT NULL';
+    const boolType = 'BOOLEAN NOT NULL';
+
+    await db.execute('''
+CREATE TABLE books (
+  id $idType,
+  name $textType,
+  author $textType,
+  status $textType
+)
+''');
+  }
+
+  Future close() async {
+    final db = await instance.database;
+    db.close();
+  }
+}
