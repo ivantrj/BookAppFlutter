4fdf61a97623fbae49e0d954c819e20ead5a2fa5
Ivan Trajanovski <ivantrajanovski@gmail.com>
2024-03-28 14:38:41 +0100

    add segmented control in library

--- lib/screens/library_page.dart
+++ lib/screens/library_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tabs_starter/data/book.dart';
 import 'package:flutter_tabs_starter/data/book_database.dart';
@@ -36,44 +37,55 @@ class _LibraryPageState extends State<LibraryPage> {
   Widget build(BuildContext context) {
     List<Book> filteredBooks = books.where((book) => book.status == selectedOption).toList();
 
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Library'),
-          bottom: TabBar(
-              tabs: const [
-                Tab(text: 'Reading'),
-                Tab(text: 'Read'),
-              ],
-              onTap: (index) {
-                setState(() {
-                  selectedOption = index == 0 ? BookStatus.reading : BookStatus.read;
-                  _loadData();
-                });
-              }),
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: CupertinoSlidingSegmentedControl<BookStatus>(
+          // thumbColor: BookStatus[sele]!,
+          groupValue: selectedOption,
+          onValueChanged: (BookStatus? value) {
+            if (value != null) {
+              setState(() {
+                selectedOption = value;
+                _loadData();
+              });
+            }
+          },
+          children: const <BookStatus, Widget>{
+            BookStatus.read: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(Icons.menu_book, color: Colors.purple),
+                  Text(
+                    'Read',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ],
+              ),
+            ),
+            BookStatus.reading: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(Icons.bookmark_added_sharp, color: Colors.purple),
+                  Text(
+                    'Reading',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ],
+              ),
+            ),
+          },
         ),
-        body: TabBarView(
-          children: [
-            // Reading Tab View
-            ListView.builder(
-              itemCount: books.length,
-              itemBuilder: (context, index) {
-                return BookTile(book: filteredBooks[index]);
-              },
-            ),
-            // Read Tab View
-            ListView.builder(
-              itemCount: books.length,
-              itemBuilder: (context, index) {
-                return BookTile(
-                  book: filteredBooks[index],
-                  // deleteFunction: (context) => deleteBook(filteredBooks[index]),
-                  // changeBookStatus: (newStatus) => changeBookStatus(filteredBooks[index], newStatus),
-                );
-              },
-            ),
-          ],
+      ),
+      child: Center(
+        child: ListView.builder(
+          itemCount: filteredBooks.length,
+          itemBuilder: (context, index) {
+            return BookTile(book: filteredBooks[index]);
+          },
         ),
       ),
     );
